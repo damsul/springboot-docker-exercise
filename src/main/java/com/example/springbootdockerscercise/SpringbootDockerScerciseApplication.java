7ccb541ebe571976b3271c7394758885d690ed9c
Damsul <gyoung9707@naver.com>
2023-05-16 14:19:53 +0900

    feat: project build

---
+++ src/main/java/com/example/springbootdockerscercise/SpringbootDockerScerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerscercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerScerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerScerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootdockerscercise/SpringbootDockerScerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerscercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerScerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
